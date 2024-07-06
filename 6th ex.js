e2b7d0b653c223b66d6b6dbf6691a7a6bee9cfc7
RAJESWARI R <91251236+Rajir-123@users.noreply.github.com>
2024-07-06 23:35:39 +0530

    '2' * 3

---
+++ 6th ex.js
@@ -0,0 +1,3 @@
+// '2' * 3 
+
+console.log('2' * 3 );//convert strings to numbers when multiplying or any numerical operation except +,because + operator used for concatenation if any value is in string.
